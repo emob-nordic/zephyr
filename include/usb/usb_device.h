ea38c9a2832e04bd85c002c203ca4ecc72cef0ad
Emil Obalski <emil.obalski@nordicsemi.no>
2020-07-07 11:09:20 +0200

    usb: Rename usb_cfg_data structure fields
    Rename USB usb_cfg_data structures to better reflect its meaning.
    
    interface -> request_handlers
        Name interface was used here as a communication interface
        between USB core stack and Class implementations.
        This naming could be missleading as USB specification defines
        interface as a part of the USB device descriptor.
    
    endpoint -> endpoints
        This change was made to reflect that there might be more than one
        endpoint.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

--- include/usb/usb_device.h
+++ include/usb/usb_device.h
@@ -183,7 +183,7 @@ struct usb_cfg_data {
 			      enum usb_dc_status_code cb_status,
 			      const uint8_t *param);
 	/** USB interface (Class) handler and storage space */
-	struct usb_interface_cfg_data interface;
+	struct usb_interface_cfg_data request_handlers;
 	/** Number of individual endpoints in the device configuration */
 	uint8_t num_endpoints;
 	/**
@@ -191,7 +191,7 @@ struct usb_cfg_data {
 	 * number of EP associated with the device description,
 	 * not including control endpoints
 	 */
-	struct usb_ep_cfg_data *endpoint;
+	struct usb_ep_cfg_data *endpoints;
 };
 
 /**
